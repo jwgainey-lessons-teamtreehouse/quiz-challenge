fae76c915e82043e666ccebac694ebd4c43112dd
Joel Gainey <jwgainey@hotmail.com>
2020-01-07 15:13:28 -0500

    quiz is functonal

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,15 @@
 var questions = [
- ['How many states are in the United States?', '50'],
- ['How many licks does it take to get to the center of a Tootsie Pop?', '3'], 
- ['What is the meaning of life?', '42']
+ ['How many states are in the United States?', 50],
+ ['How many licks does it take to get to the center of a Tootsie Pop?', 3], 
+ ['What is the meaning of life?', 42]
 ];
 
-var correctAnswer = 0;
+var correctAnswers = 0;
 var question;
 var answer;
 var response;
 var html;
+
 function print(message) {
     document.write(message);
   }
@@ -16,6 +17,12 @@ function print(message) {
   for (var i = 0; i < questions.length; i += 1 ) {
       question = questions[i][0];
       answer = questions[i][1];
-      response = parseInt(prompt(questions));
+      response = parseInt(prompt(question));
+      if ( response === answer) {
+          correctAnswers += 1;
+      }
   }
+
+  html = "You got " + correctAnswers + " questions(s) right!";
+  print(html);
   
